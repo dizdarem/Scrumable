3da54200c944515028330e2225c1bb40a0ab17ee
Sandro <asseks@edu.htl-villach.at>
2019-04-28 18:43:31 +0200

    Implemented Toolbar with Back Arrow in DetailsActivity

--- app/src/main/java/at/htl_villach/scrumable/app/DetailsActivity.java
+++ app/src/main/java/at/htl_villach/scrumable/app/DetailsActivity.java
@@ -3,6 +3,11 @@ package at.htl_villach.scrumable.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import at.htl_villach.scrumable.R;
@@ -10,16 +15,31 @@ import at.htl_villach.scrumable.bll.BacklogItem;
 
 public class DetailsActivity extends AppCompatActivity {
     private TextView tv;
+    private ImageView imageBtn;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
         tv = (TextView)findViewById(R.id.tv);
+        imageBtn = (ImageView)findViewById(R.id.imageBtnBack);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+
+        imageBtn.bringToFront();
+        toolbar.invalidate();
 
         Intent intent = getIntent();
         BacklogItem backlogItem = intent.getParcelableExtra("selectedListItemObj");
 
         tv.setText(backlogItem.toString());
+
+        imageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DetailsActivity.this.finish();
+            }
+        });
     }
 }
