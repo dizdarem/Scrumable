d16ae8f4463737543acdbb37d23ac9695cf02f60
dizdarem <meli.dizdarevic@gmail.com>
2019-04-30 10:49:29 +0200

    added options thing in order to edit user story

--- app/src/main/java/at/htl_villach/scrumable/app/DetailsActivity.java
+++ app/src/main/java/at/htl_villach/scrumable/app/DetailsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -49,5 +50,12 @@ public class DetailsActivity extends AppCompatActivity {
                 DetailsActivity.this.finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.option_edituserstory, menu);
+        return true;
     }
 }
--- app/src/main/java/at/htl_villach/scrumable/app/ScrumActivity.java
+++ app/src/main/java/at/htl_villach/scrumable/app/ScrumActivity.java
@@ -55,7 +55,7 @@ public class ScrumActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.start, menu);
+        getMenuInflater().inflate(R.menu.options, menu);
         return true;
     }
 
