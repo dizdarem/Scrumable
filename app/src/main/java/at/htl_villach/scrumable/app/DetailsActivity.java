4e5a2d6075519de956f2cbd2325a821308537f1f
dizdarem <meli.dizdarevic@gmail.com>
2019-05-02 09:39:19 +0200

    added option edituserstory, changed the actionbar and made an action on the menuitem

--- app/src/main/java/at/htl_villach/scrumable/app/DetailsActivity.java
+++ app/src/main/java/at/htl_villach/scrumable/app/DetailsActivity.java
@@ -5,9 +5,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import at.htl_villach.scrumable.R;
 import at.htl_villach.scrumable.bll.BacklogItem;
@@ -24,6 +26,7 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        setTitle("");
 
         initControls();
     }
@@ -35,6 +38,7 @@ public class DetailsActivity extends AppCompatActivity {
         tvDescription = (TextView)findViewById(R.id.tvDescription);
         imageBtn = (ImageView)findViewById(R.id.imageBtnBack);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
         BacklogItem backlogItem = intent.getParcelableExtra("selectedListItemObj");
@@ -57,5 +61,19 @@ public class DetailsActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.option_edituserstory, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.mitem_EditUserStory) {
+            tvTitle = (TextView)findViewById(R.id.tvTitle);
+            tvEditor = (TextView)findViewById(R.id.tvEditor);
+            tvStatus = (TextView)findViewById(R.id.tvStatus);
+            tvDescription = (TextView)findViewById(R.id.tvDescription);
+            tvEditor.setEnabled(true);
+            Toast.makeText(DetailsActivity.this,"Action clicked",Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
